88cc196d1b81c6d3e56efd4a7990a22359cc2364
Lucas chagas <lucazchagaz@outlook.com>
2023-10-27 01:31:51 -0300

    feat: add DemandItem component - its a component to show a information about a project

---
+++ src/components/DemandItem/DemandItem.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Icon } from "../Icon";
+import { DemandWrapper } from "./styled";
+import { Box } from "../Box/Box";
+import { Text } from "../Text/Text";
+
+export const DemandItem = () => {
+  return (
+    <DemandWrapper>
+      <Box style={{ flex: 1, flexDirection: "row", alignItems: "center" }}>
+        <Icon name="calendar" />
+        <Box style={{ marginLeft: 16 }}>
+          <Text bold>Projeto1</Text>
+          <Box style={{ flexDirection: "row" }}>
+            <Text
+              color="Icon"
+              variant="subTitleOne"
+              bold
+              style={{ marginRight: 13 }}
+            >
+              10:00 AM
+            </Text>
+            <Text color="Icon" variant="subTitleOne" bold>
+              finalizado
+            </Text>
+          </Box>
+        </Box>
+      </Box>
+      <Icon name="chevronRigth" size={15} />
+    </DemandWrapper>
+  );
+};
---
+++ src/components/DemandItem/styled.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components/native";
+
+export const DemandWrapper = styled.TouchableOpacity`
+  width: 100%;
+  height: 75px;
+  align-items: center;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 14px 26px 14px 24px;
+`;
