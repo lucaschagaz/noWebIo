46fa3aa037987d894edd5395f51c09d7a76292cd
Lucas chagas <lucazchagaz@outlook.com>
2023-10-27 01:30:02 -0300

    refector: add red color with Alert name

--- src/Styles/Theme/theme.ts
+++ src/Styles/Theme/theme.ts
@@ -2,10 +2,11 @@ export const Theme = {
   colors: {
     Primary: "#1BD15D",
     Primary_Hard: "#088435",
-    Primary_100: "#87E4A7C7",
+    Primary_100: "#87E4A7",
     PrimaryContrasct: "#FFFFFF",
     Icon: "#9B9B9B",
     BackgroundContrasct: "#0A0909",
+    Alert: "#EC7669",
   },
 };
 
