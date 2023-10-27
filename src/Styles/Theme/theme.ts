108f900fac0b85b3933868f0c72f4ac45ce3ef99
Lucas chagas <lucazchagaz@outlook.com>
2023-10-27 11:02:48 -0300

    feat: add new color

--- src/Styles/Theme/theme.ts
+++ src/Styles/Theme/theme.ts
@@ -4,6 +4,7 @@ export const Theme = {
     Primary_Hard: "#088435",
     Primary_100: "#87E4A7",
     PrimaryContrasct: "#FFFFFF",
+    Primary_opacity: "#EFFCF4",
     Icon: "#9B9B9B",
     BackgroundContrasct: "#0A0909",
     Alert: "#EC7669",
