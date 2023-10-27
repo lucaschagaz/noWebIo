c9ff1c4561effc12f38d75ef31d1896fa5634964
Lucas chagas <lucazchagaz@outlook.com>
2023-10-27 11:03:53 -0300

    style: add padding bottom

--- src/components/Screen/Screen.tsx
+++ src/components/Screen/Screen.tsx
@@ -4,13 +4,20 @@ import {
   ScrollViewComponent,
   ViewComponent,
 } from "./ScreenComponent/ScreenComponent";
+import { Icon } from "../Icon";
+import { TouchableOpacity } from "react-native";
 
 export interface IScreenProps {
   children: React.ReactNode;
   scrolable?: boolean;
+  canGoBack?: boolean;
 }
 
-export const Screen = ({ children, scrolable }: IScreenProps) => {
+export const Screen = ({
+  children,
+  scrolable,
+  canGoBack = false,
+}: IScreenProps) => {
   const Component = scrolable ? ScrollViewComponent : ViewComponent;
   return (
     <Component>
@@ -18,8 +25,14 @@ export const Screen = ({ children, scrolable }: IScreenProps) => {
         style={{
           marginHorizontal: 28,
           marginTop: 52,
+          marginBottom: 20,
         }}
       >
+        {canGoBack && (
+          <TouchableOpacity>
+            <Icon name="leftSet" size={18} />
+          </TouchableOpacity>
+        )}
         {children}
       </Box>
     </Component>
