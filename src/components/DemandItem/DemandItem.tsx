744a1ef8ca2f7c393dbf250020743474eb7cd960
Lucas chagas <lucazchagaz@outlook.com>
2023-10-28 11:06:46 -0300

    feat: add interface and props for get project data

--- src/components/DemandItem/DemandItem.tsx
+++ src/components/DemandItem/DemandItem.tsx
@@ -3,15 +3,22 @@ import { Icon } from "../Icon";
 import { DemandWrapper } from "./styled";
 import { Box } from "../Box/Box";
 import { Text } from "../Text/Text";
+import { IProject } from "../../store/usePorjects";
 
-export const DemandItem = () => {
+export const DemandItem = ({
+  name,
+  status,
+  time,
+  urgency,
+  notifications,
+}: IProject) => {
   return (
     <DemandWrapper>
       <Box style={{ flex: 1, flexDirection: "row", alignItems: "center" }}>
         <Icon name="bussines" />
         <Box style={{ marginLeft: 16 }}>
           <Text bold variant="ParagraphTwo">
-            Projeto1
+            {name}
           </Text>
           <Box style={{ flexDirection: "row" }}>
             <Text
@@ -23,7 +30,7 @@ export const DemandItem = () => {
               10:00 AM
             </Text>
             <Text color="Icon" variant="subTitleOne" bold>
-              finalizado
+              {status}
             </Text>
           </Box>
         </Box>
