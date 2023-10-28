34e1e51c2925459ce5288b4f8c44acebe775f69d
Lucas chagas <lucazchagaz@outlook.com>
2023-10-28 11:07:21 -0300

    refector: add activityIndicator

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,24 +2,30 @@ import React from "react";
 import { Wrapper } from "./styled";
 import { Text as RNText } from "../Text/Text";
 import { ThemeColor } from "../../Styles/Theme/theme";
-import { TouchableOpacityProps } from "react-native";
+import { ActivityIndicator, TouchableOpacityProps } from "react-native";
 
 interface IButtonProps extends TouchableOpacityProps {
   color?: keyof ThemeColor;
   text: string;
+  loading?: boolean;
 }
 
 export const Button = ({
   color = "Primary",
   style,
+  loading,
   text,
   ...rest
 }: IButtonProps) => {
   return (
     <Wrapper color={color} {...rest} style={style}>
-      <RNText variant="headingThree" bold color="PrimaryContrasct">
-        {text}
-      </RNText>
+      {loading ? (
+        <ActivityIndicator />
+      ) : (
+        <RNText variant="headingThree" bold color="PrimaryContrasct">
+          {text}
+        </RNText>
+      )}
     </Wrapper>
   );
 };
