3ae2c85776d18d2080a0c570267b3b7671818379
Lucas chagas <lucazchagaz@outlook.com>
2023-10-28 11:08:16 -0300

    refector: remove filds from FormSchema

--- src/@types/types.ts
+++ src/@types/types.ts
@@ -1,18 +1,13 @@
 import * as yup from "yup";
 
 export interface IFormProps {
-  hour?: string;
-  minute?: string;
+  // hour?: string;
+  // minute?: string;
   projectName?: string;
   amount?: string;
 }
 
 export const FormSchema: yup.ObjectSchema<IFormProps> = yup.object().shape({
-  hour: yup.string().max(2, "Maximo de 2 letras").required("Campo obrigatorio"),
-  minute: yup
-    .string()
-    .max(2, "Maximo de 2 letras")
-    .required("Campo obrigatorio"),
   projectName: yup
     .string()
     .min(5, "no minimo 5 letras")
